e56cec1fd95d46d9aeca777ec5c8613a9c646e19
anhsonit <leanhson.uet@gmail.com>
2017-03-10 19:12:40 +0700

    login screen

--- Client-iOS/CrossWorld/CrossWorld/View/Login/LoginViewController.swift
+++ Client-iOS/CrossWorld/CrossWorld/View/Login/LoginViewController.swift
@@ -8,28 +8,34 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
-
+class LoginViewController: AppViewController {
+    
+    // MARK: - Outlet
+    
+    // MARK: - Declare
+    
+    // MARK: - Define
+    
+    // MARK: - Setup
+    
+    // MARK: - Lifecircle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - AppViewController
+    override func setupViewController() {
+        self.typeViewController = .root
+        self.typeNavigationBar = .hidden
     }
-    */
-
+    
+    
 }
